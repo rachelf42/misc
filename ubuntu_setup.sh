7fae0b2b7f7b36a3f138782ef70e4a662b774cc9
Rachel <59665717+rachelf42@users.noreply.github.com>
2025-03-07 10:34:28 -0800

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -10,71 +10,57 @@ function yes_or_no {
     done
 }
 
-function vbox_guest_additions {
-        apt-get install gcc make perl bzip2 tar
-	umount /mnt
-	mount /dev/cdrom /mnt
-	bash /mnt/VBoxLinuxAdditions.run
-	umount /mnt
-}
-
-function do_ssh_keys {
-	wget -O /home/rachel/.ssh/authorized_keys https://github.com/rachelf42.keys
- 	chown rachel:rachel /home/rachel/.ssh/authorized_keys
- 	wget -O /root/.ssh/authorized_keys https://github.com/rachelf42.keys
-}
-
-function make_user {
-	apt-get install -y sudo
- 	adduser rachel
-  	usermod -aG sudo rachel
-   	mkdir /home/rachel/.ssh
-    	chown rachel:rachel /home/rachel/.ssh
-     	chmod 700 /home/rachel/.ssh
-}
-
 apt-get update
 
-yes_or_no "Do we need to install the non-root user?" && make_user
+if id "rachel" >/dev/null 2>&1; then
+    echo 'user found'
+else
+    echo 'user not found'
+    apt-get install -y sudo
+    adduser rachel
+    usermod -aG sudo rachel
+    mkdir /home/rachel/.ssh
+    chown rachel:rachel /home/rachel/.ssh
+    chmod 700 /home/rachel/.ssh
+    wget -O /home/rachel/.ssh/authorized_keys https://github.com/rachelf42.keys
+fi
+
+rm /root/.ssh/authorized_keys
+cat /home/rachel/.ssh/authorized_keys > /root/.ssh/authorized_keys
 
 chmod -x /etc/update-motd.d/10-help-text
 chmod -x /etc/update-motd.d/50-motd-news
 
-echo 'force_color_prompt=yes' >> /home/rachel/.bashrc
 cat <<'EOD' >> /home/rachel/.bashrc
+force_color_prompt=yes
 export PS1='[$?] \[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[35m\]\w\[\e[m\] \\$> '
 EOD
-echo 'set tabsize 4' >> /home/rachel/.nanorc
-echo 'set tabstospaces' >> /home/rachel/.nanorc
-echo 'set autoindent' >> /home/rachel/.nanorc
+NANORC=<<EOD
+set tabsize 4
+set tabstospaces
+set autoindent
+EOD
+echo "$NANORC" >> /home/rachel/.bashrc
 
-echo 'force_color_prompt=yes' >> /root/.bashrc
 cat <<'EOD' >> /root/.bashrc
+force_color_prompt=yes
 export PS1='[$?] \[\e[31m\]\u\[\e[m\]\[\e[31m\]@\[\e[m\]\[\e[31m\]\h\[\e[m\]:\[\e[35m\]\w\[\e[m\] \\$> '
 EOD
-echo 'set tabsize 4' >> /root/.nanorc
-echo 'set tabstospaces' >> /root/.nanorc
-echo 'set autoindent' >> /root/.nanorc
+echo "$NANORC" >> /root/.nanorc
 
 echo 'force_color_prompt=yes' >> /etc/skel/.bashrc
 cat <<'EOD' >> /etc/skel/.bashrc
-export PS1=\'[$?] \[\e[33m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[33m\]\h\[\e[m\]:\[\e[35m\]\w\[\e[m\] \\$> '
+force_color_prompt=yes
+export PS1='[$?] \[\e[33\]\u\[\e[m\]\[\e[33\]@\[\e[m\]\[\e[33\]\h\[\e[m\]:\[\e[35m\]\w\[\e[m\] \\$> '
 EOD
-echo 'set tabsize 4' >> /etc/skel/.nanorc
-echo 'set tabstospaces' >> /etc/skel/.nanorc
-echo 'set autoindent' >> /etc/skel/.nanorc
+echo "$NANORC" >> /etc/skel/.nanorc
 
-yes_or_no "Do we need trash-cli? (y for server, n for desktop)" && apt -y install trash-cli && echo "alias rm='echo \"use trash, dummy\"; false'" >> /home/rachel/.bashrc
+if dpkg -l gedit >/dev/null 2>&1; then
+    apt -y install trash-cli && echo "alias rm='echo \"use trash, dummy\"; tput bel; false'" >> /home/rachel/.bashrc
+fi
+printf 'America\nVancouver\n' | dpkg-reconfigure -f teletype tzdata > /dev/null 2>&1
 
-yes_or_no "Do we need to set timezone?" && dpkg-reconfigure tzdata
-
-yes_or_no "Do we need to download SSH keys?" && do_ssh_keys
-
-yes_or_no "Do we need VBox Guest Additions? (if yes insert disk before proceeding)" && vbox_guest_additions
-
-yes_or_no "Do we need QEmu Guest Agent?"  && apt install -y qemu-guest-agent
-
-yes_or_no "Fuck snapd?" && apt-mark hold snapd && tee /etc/apt/preferences.d/00-fuck-snapd <<EOD
+apt-mark hold snapd && tee /etc/apt/preferences.d/00-fuck-snapd <<EOD
 Package: snapd
 Pin: release *
 Pin-Priority: -1
@@ -85,4 +71,4 @@ sleep 10
 apt-get -y upgrade
 
 echo 'Finished! Rebooting...'
-shutdown --reboot +3
+shutdown --reboot +5
